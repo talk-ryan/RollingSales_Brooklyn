c776bc98dcb8638569fb82db56e16b32136fd65f
UbuntuVM <ryan.a.talk@gmail.com>
2017-09-27 19:48:28 -0700

    Add paper directory and Data Patterns file for the project structure. Require  gdata and plyr packages in R_rollingsales_brooklyn.R

--- source/R _rollingsales_brooklyn.R
+++ source/R _rollingsales_brooklyn.R
@@ -1,8 +1,8 @@
 # Author: Benjamin Reddy
 # Taken from pages 49-50 of O'Neil and Schutt
 
-#require(gdata)
-#require(plyr) #Added by Monnie McGee
+require(gdata)
+require(plyr) #Added by Monnie McGee
 #install the gdata and plyr packages and load in to R.
 library(plyr)
 library(gdata)
